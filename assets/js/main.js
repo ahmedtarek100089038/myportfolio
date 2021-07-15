4da9c52820b9be6ecfdd933efcf9dc6d6e12f66c
ahmedtarek100089038 <55531401+ahmedtarek100089038@users.noreply.github.com>
2021-07-16 07:07:42 +0800

    Add Preloader Function

--- assets/js/main.js
+++ assets/js/main.js
@@ -180,3 +180,20 @@ themeButton.addEventListener('click', () => {
 
 
 
+$(document).ready(function(){
+  // typing text animation script
+  var typed = new Typed(".typing", {
+      strings: ["Web Developer", "Front End Developer", "Back End Developer", "Web Designer", "UI-UX Developer"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+  });
+
+  var typed = new Typed(".typing-2", {
+      strings: ["Web Developer", "Front End Developer", "Back End Developer", "Web Designer", "UI-UX Developer"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+  });
+
+});
